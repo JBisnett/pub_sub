2dc8c3dda408ebed01115697c68bc22918b6c31a
Jacob Bisnett <jacob.bisnett@gmail.com>
2015-11-19 20:47:35 -0500

    mostly works for files

--- gen-rb/broker_handler.rb
+++ gen-rb/broker_handler.rb
@@ -8,18 +8,21 @@ class BrokerHandler
   # stores the hosts and ports that have registered for each topic
   @topic_hash
   def initialize()
-    @topic_hash = Hash.new []
+    @topic_hash = Hash.new Set.new
   end
   # add host and port to a topic
   def subscribe(topic, host, port)
     @topic_hash[topic] << [host, port]
+    puts "#{host}: #{port} subscribes to #{topic}"
   end
   # remove host and port from a topic
   def unsubscribe(topic, host, port)
     @topic_hash[topic].delete [host,port]
+    puts "#{host}: #{port} unsubscribes from #{topic}"
   end
   # sends the topic and message to each subscriber
   def publish(topic, message)
+    puts "publish #{topic}: #{message}"
     @topic_hash[topic].each do |arr|
       host = arr[0]
       port = arr[1]
--- gen-rb/consumer.rb
+++ gen-rb/consumer.rb
@@ -8,28 +8,36 @@ require_relative 'consumer_handler'
 require_relative 'broker_handler'
 
 begin
-	broker_host = 'localhost' || ARGV.shift
-	broker_port = 9090 || ARGV.shift
-
-	transport = Thrift::BufferedTransport.new(Thrift::Socket.new(broker_host, broker_port))
-	protocol = Thrift::BinaryProtocol.new(transport)
-	client = Concord::PubSub::PubSubBroker::Client.new(protocol)
-
-	transport.open()
-
 	#read possible input
+	transport = nil
+	$client = nil
+	$broker_host = nil
+	$broker_port = nil
+	$local_host = nil
+	$local_port = nil
 	ARGF.readlines.each do |line|
-		if /sub,\s?(\w*),\s?(\w*),\s?(\d*)/ =~ line
-			client.subscribe $~[1], $~[2], $~[3].to_i
-		elsif /unsub,\s?(\w*),\s?(\w*),\s?(\d*)/ =~ line
-			client.unsubscribe $~[1], $~[2], $~[3].to_i
-		elsif /pub,\s?(\w*),\s?(\w*)/ =~ line
-			client.publish $~[1], $~[2]
+		if /\Abroker:\s?(\w*),\s?(\d*)/ =~ line
+			transport.close if transport
+			$broker_host = $~[1]
+			$broker_port = $~[2].to_i
+			transport = Thrift::BufferedTransport.new(Thrift::Socket.new($broker_host, $broker_port))
+			protocol = Thrift::BinaryProtocol.new(transport)
+			$client = Concord::PubSub::PubSubBroker::Client.new(protocol)
+			transport.open()
+		elsif /\Alocal:\s?(\w*),\s?(\d*)/ =~ line
+			$local_host = $~[1]
+			$local_port = $~[2].to_i
+		elsif /\Asub,\s?(\w*)/ =~ line
+			$client.subscribe $~[1], $local_host, $local_port
+		elsif /\Aunsub,\s?(\w*)/ =~ line
+			$client.unsubscribe $~[1], $local_host, $local_port
+		elsif /\Apub,\s?(\w*),\s?(\w*)/ =~ line
+			$client.publish $~[1], $~[2]
 		else
 			puts "Invalid Input"
 		end
 	end
-	transport.close()
+	transport.close if transport
 
 rescue
 	puts $!, $!.message
